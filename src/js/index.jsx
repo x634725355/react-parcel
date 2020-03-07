ef7340e791841d8f955cb29ac83b2b045ca28a6c
CSC <634725355@qq.com>
2020-03-07 18:12:51 +0800

    添加swiper

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// swiper引入
+import 'swiper/js/swiper.min.js';
+import 'swiper/css/swiper.css';
+
+
 import './index.less';
+
 
 import { App } from './App';
 
